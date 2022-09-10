4709907ca8e1ec51e6e7cc3a8492ce2c4a2645d9
Daniels Hogans <daniel.santos.ap@gmail.com>
2022-09-10 14:45:03 -0300

    wow

--- resources/views/bootstrap/landingpage.blade.php
+++ resources/views/bootstrap/landingpage.blade.php
@@ -18,7 +18,7 @@
   </header>
 
 <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <img class="d-block mx-auto mb-4" src="icon.png" alt="" height="75">
     <h1 class="display-5 fw-bold">Laravel Fullstack</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">O Framework de desenvolvimento mais robusto e procurado do mercado, pagando saláriosde até <b>R$ 20.000 </b><i>(vinte mil)</i> / mês, para desenvolvedores sêniors. 
